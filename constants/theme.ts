002fb7115dbffd1740c3cc7117de46c84dbc31da
segev perets <segev-p@yit.co.il>
2026-06-09 19:07:44 +0300

    initial commit

---
+++ constants/theme.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  background: '#fff',
+  text: '#FFE81F',
+  primary: '#01BEE5',
+  secondary: '#0094DD',
+  dark: '#000000',
+  light: '#e6e6e6',
+  primaryLight: '#E9F9FF',
+  muted: '#666',
+};
+
+export const Fonts = {
+  brand: 'Nunito',
+  brandBold: 'Nunito_700Bold',
+  brandBlack: 'Nunito_900Black',
+};
